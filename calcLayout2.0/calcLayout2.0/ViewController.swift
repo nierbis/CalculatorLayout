ed0f8bf60d67cd0503ae9872e86fe66292082988
Arturas <zubearturas@gmail.com>
2019-09-15 22:06:32 +0300

    Add files via upload

---
+++ calcLayout2.0/calcLayout2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calcLayout2.0
+//
+//  Created by Arturas Zube on 15/9/19.
+//  Copyright © 2019 Arturas Zube. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
